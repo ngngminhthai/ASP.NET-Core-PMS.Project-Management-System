d5ddf1f1b35b84b8341b92456d94ffb219c8cd6c
nguyenminhthaijs <thainguyenminh441@gmail.com>
2023-02-01 12:18:01 +0700

    Completed login function

---
+++ PMS.Data/Entities/ValueObjects/Priority.cs
@@ -0,0 +1,16 @@
+﻿public class Priority
+{
+    public string Name { get; private set; }
+    public int Level { get; private set; }
+    public string ColorCode { get; private set; }
+
+    public Priority(string name, int level, string colorCode)
+    {
+        Name = name;
+        Level = level;
+        ColorCode = colorCode;
+    }
+    public static Priority High = new Priority("High", 3, "Red");
+    public static Priority Medium = new Priority("Medium", 2, "Yellow");
+    public static Priority Low = new Priority("Low", 1, "Green");
+}
