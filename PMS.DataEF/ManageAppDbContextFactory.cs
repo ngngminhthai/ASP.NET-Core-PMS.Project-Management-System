73c69577bf23dfa5233099d1bf8b9497a45c9e4a
nguyenminhthaijs <thainguyenminh441@gmail.com>
2023-02-08 15:47:24 +0700

    Untrack files

---
+++ PMS.DataEF/ManageAppDbContextFactory.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using WebApplication1.Data;
+
+public class ManageAppDbContextFactory : IDesignTimeDbContextFactory<ManageAppDbContext>
+{
+    ManageAppDbContext IDesignTimeDbContextFactory<ManageAppDbContext>.CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<ManageAppDbContext>();
+        optionsBuilder.UseSqlServer("Server=.;database=SignalRCRUD;user=sa;password=sa;TrustServerCertificate=true");
+
+        return new ManageAppDbContext(optionsBuilder.Options);
+    }
+}
